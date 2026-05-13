55e9e3262ba3584c6e0d81177f0d1705afd80e52
RaffyMont <raffymont10@gmail.com>
2026-05-13 20:25:08 +0200

    Creazione bean ColoreBean

---
+++ src/main/java/it/unisa/storage/model/ColoreBean.java
@@ -0,0 +1,26 @@
+package it.unisa.storage.model;
+
+import java.io.Serializable;
+
+public class ColoreBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String colore;
+	
+	public ColoreBean()
+	{
+		
+	}
+
+	public String getColore() {
+		return colore;
+	}
+
+	public void setColore(String colore) {
+		this.colore = colore;
+	}
+	
+	
+	
+}
