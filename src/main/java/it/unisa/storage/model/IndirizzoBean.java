8dfd9f87a207b1ccc31698fde063d90d963d6884
RaffyMont <raffymont10@gmail.com>
2026-05-13 18:21:08 +0200

    creazione IndirizzoBean

---
+++ src/main/java/it/unisa/storage/model/IndirizzoBean.java
@@ -0,0 +1,90 @@
+package it.unisa.storage.model;
+
+import java.io.Serializable;
+
+public class IndirizzoBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int id_indirizzo;
+    private String provincia;
+    private String stato;
+	private String citta;
+    private String CAP;
+    private String via;
+    private String civico;
+    
+    public IndirizzoBean()
+    {
+    	
+    }
+
+	public int getId_indirizzo() {
+		return id_indirizzo;
+	}
+
+	public void setId_indirizzo(int id_indirizzo) {
+		this.id_indirizzo = id_indirizzo;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getStato() {
+		return stato;
+	}
+
+	public void setStato(String stato) {
+		this.stato = stato;
+	}
+
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+	public String getCAP() {
+		return CAP;
+	}
+
+	public void setCAP(String cAP) {
+		CAP = cAP;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public String getCivico() {
+		return civico;
+	}
+
+	public void setCivico(String civico) {
+		this.civico = civico;
+	}
+    
+    
+    
+    /*
+    	Serve l'oggetto Immagine
+    public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+
+	public boolean hasImage() {
+        return path != null && !path.isEmpty();
+    }
+    */
+}
