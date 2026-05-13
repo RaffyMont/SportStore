3852c4aa1f2aefac8f492213efbe144bee3fdf8a
RaffyMont <raffymont10@gmail.com>
2026-05-13 20:43:23 +0200

    fatto diventare il bean astratto

--- src/main/java/it/unisa/storage/model/ProdottoBean.java
+++ src/main/java/it/unisa/storage/model/ProdottoBean.java
@@ -2,7 +2,7 @@ package it.unisa.storage.model;
 
 import java.io.Serializable;
 
-public class ProdottoBean implements Serializable{
+public abstract class ProdottoBean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
