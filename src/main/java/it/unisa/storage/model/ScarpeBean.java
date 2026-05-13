5c16d985aa12f2a4eebe9012d60a8774849d04fa
RaffyMont <raffymont10@gmail.com>
2026-05-13 23:26:59 +0200

    cambiamenti in ScarpeBean.java

--- src/main/java/it/unisa/storage/model/ScarpeBean.java
+++ src/main/java/it/unisa/storage/model/ScarpeBean.java
@@ -1,10 +1,33 @@
 package it.unisa.storage.model;
 
-
+/* DA RIVEDERE MEGLIO */
 public class ScarpeBean extends ProdottoBean{
 
+	private static final long serialVersionUID = 1L;
+	
 	private String tipo_suola;
 	private String materiale;
+	
+	public ScarpeBean()
+	{
+		
+	}
+
+	public String getTipo_suola() {
+		return tipo_suola;
+	}
+
+	public void setTipo_suola(String tipo_suola) {
+		this.tipo_suola = tipo_suola;
+	}
+
+	public String getMateriale() {
+		return materiale;
+	}
+
+	public void setMateriale(String materiale) {
+		this.materiale = materiale;
+	}
 	
 	
 }
