b3bd8d9e1d5f61b28eaaae8527d9f0c4cd27d335
RaffyMont <raffymont10@gmail.com>
2026-05-13 23:12:39 +0200

    creazione bean SupportoTagliaBean.java

---
+++ src/main/java/it/unisa/storage/model/SupportoTagliaBean.java
@@ -0,0 +1,34 @@
+package it.unisa.storage.model;
+
+import java.io.Serializable;
+
+public class SupportoTagliaBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private ProdottoBean id_prodotto;
+	private TagliaBean taglia;
+	
+	public SupportoTagliaBean()
+	{
+		
+	}
+
+	public ProdottoBean getId_prodotto() {
+		return id_prodotto;
+	}
+
+	public void setId_prodotto(ProdottoBean id_prodotto) {
+		this.id_prodotto = id_prodotto;
+	}
+
+	public TagliaBean getTaglia() {
+		return taglia;
+	}
+
+	public void setTaglia(TagliaBean taglia) {
+		this.taglia = taglia;
+	}
+	
+	
+}
