8214a2a3e9ec972047c3e18aaea9fa7a50e50c20
RaffyMont <raffymont10@gmail.com>
2026-05-13 20:41:36 +0200

    creazione bean UtenteBean in model

---
+++ src/main/java/it/unisa/storage/model/UtenteBean.java
@@ -0,0 +1,89 @@
+package it.unisa.storage.model;
+
+import java.io.Serializable;
+
+public class UtenteBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String is_utente;
+	private String nome;
+	private String cognome;
+    private String email;
+    private String password; //non so se serve
+    private enum Ruolo{admin, common};
+    private Ruolo ruolo;
+    private String cellulare;
+    private IndirizzoBean indirizzo;
+    
+    public UtenteBean()
+    {
+    	
+    }
+
+	public String getIs_utente() {
+		return is_utente;
+	}
+
+	public void setIs_utente(String is_utente) {
+		this.is_utente = is_utente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Ruolo getRuolo() {
+		return ruolo;
+	}
+
+	public void setRuolo(Ruolo ruolo) {
+		this.ruolo = ruolo;
+	}
+
+	public String getCellulare() {
+		return cellulare;
+	}
+
+	public void setCellulare(String cellulare) {
+		this.cellulare = cellulare;
+	}
+
+	public IndirizzoBean getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(IndirizzoBean indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+    
+    
+}
