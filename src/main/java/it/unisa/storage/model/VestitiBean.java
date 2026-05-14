6b78b17b115165a2de8110d2fd2b4bc1bbeb61c4
RaffyMont <raffymont10@gmail.com>
2026-05-14 08:31:26 +0200

    creazione bean VestitiBean.java nel package model

---
+++ src/main/java/it/unisa/storage/model/VestitiBean.java
@@ -0,0 +1,81 @@
+package it.unisa.storage.model;
+
+public class VestitiBean extends ProdottoBean{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String tipovita;
+	private String tessuto;
+	private String stagione;
+	private enum Categoria{MAGLIETTA, PANTALONE, TUTA};
+	private Categoria categoria;
+	private String tipo_collo;
+	private enum Manica{CORTA, LUNGA};
+	private Manica manica;
+	private enum Gamba{CORTI, LUNGHI};
+	private Gamba gamba;
+   
+	public VestitiBean()
+	{
+		
+	}
+
+	public String getTipovita() {
+		return tipovita;
+	}
+
+	public void setTipovita(String tipovita) {
+		this.tipovita = tipovita;
+	}
+
+	public String getTessuto() {
+		return tessuto;
+	}
+
+	public void setTessuto(String tessuto) {
+		this.tessuto = tessuto;
+	}
+
+	public String getStagione() {
+		return stagione;
+	}
+
+	public void setStagione(String stagione) {
+		this.stagione = stagione;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getTipo_collo() {
+		return tipo_collo;
+	}
+
+	public void setTipo_collo(String tipo_collo) {
+		this.tipo_collo = tipo_collo;
+	}
+
+	public Manica getManica() {
+		return manica;
+	}
+
+	public void setManica(Manica manica) {
+		this.manica = manica;
+	}
+
+	public Gamba getGamba() {
+		return gamba;
+	}
+
+	public void setGamba(Gamba gamba) {
+		this.gamba = gamba;
+	}
+	
+	
+
+}
