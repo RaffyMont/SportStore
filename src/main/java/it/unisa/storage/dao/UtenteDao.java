0486b520b39cf124a82a1c472a1a077ac82c06f3
RaffyMont <raffymont10@gmail.com>
2026-05-16 12:54:50 +0200

    creazione interfaccia UtenteDAO

---
+++ src/main/java/it/unisa/storage/dao/UtenteDao.java
@@ -0,0 +1,24 @@
+package it.unisa.storage.dao;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import it.unisa.storage.model.UtenteBean;
+
+public interface UtenteDao {
+	
+	public void doSave(UtenteBean utente) throws SQLException;
+	
+	public boolean doUpdate(UtenteBean utente) throws SQLException;
+	
+	public boolean doDelete(String id_utente) throws SQLException;
+	
+	public UtenteBean doRetrieveByKey(String id_utente) throws SQLException;
+	
+	public UtenteBean doRetreiveByEmail(String email) throws SQLException;
+	
+	public UtenteBean doAuthenticate(String email, String pwd) throws SQLException;
+	
+	public Collection<UtenteBean> doRetreiveAll(String order) throws SQLException;
+
+}
