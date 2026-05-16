1218586e2b0b90dcf6f6a0a1a7aab0645be15605
RaffyMont <raffymont10@gmail.com>
2026-05-16 11:53:54 +0200

    creazione indirizzoDao

--- src/main/java/it/unisa/storage/dao/IndirizzoDao.java
+++ src/main/java/it/unisa/storage/dao/IndirizzoDao.java
@@ -1,7 +1,20 @@
 package it.unisa.storage.dao;
+
 import java.sql.SQLException;
 
-public class IndirizzoDao {
-	
-	
+import java.util.Collection;
+
+import it.unisa.storage.model.IndirizzoBean;
+
+public interface IndirizzoDao {
+
+    void doSave(IndirizzoBean indirizzo) throws SQLException;
+
+    boolean doUpdate(IndirizzoBean indirizzo) throws SQLException;
+
+    boolean doDelete(int id_indirizzo) throws SQLException;
+
+    IndirizzoBean doRetrieveByKey(int id_indirizzo) throws SQLException;
+
+    Collection<IndirizzoBean> doRetrieveAll(String order) throws SQLException;
 }
