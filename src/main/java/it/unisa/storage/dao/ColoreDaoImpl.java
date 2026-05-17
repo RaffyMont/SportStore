2b9c1f694bf74bc88e8a97fe25fd43b7f4191b1d
RaffyMont <raffymont10@gmail.com>
2026-05-17 21:44:22 +0200

    implementazione metodo doRetrieveAll nella classe ColoreDaoImpl

--- src/main/java/it/unisa/storage/dao/ColoreDaoImpl.java
+++ src/main/java/it/unisa/storage/dao/ColoreDaoImpl.java
@@ -4,13 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import it.unisa.storage.model.ColoreBean;
-import it.unisa.storage.model.ProdottoBean.Categoria;
-import it.unisa.storage.model.ProdottoBean.Genere;
 
 public class ColoreDaoImpl implements ColoreDao{
 	
@@ -59,5 +58,22 @@ public class ColoreDaoImpl implements ColoreDao{
         return bean;
     }
     
-    public Collection<ColoreBean> doRetrieveAll(String order) throws SQLException;
+    public synchronized List<ColoreBean> doRetrieveAll(String order) throws SQLException
+    {
+    	List<ColoreBean> colori = new LinkedList<ColoreBean>();
+    	String selectSQL = "SELECT * FROM " + TABLE_NAME;
+        if (order != null && !order.isEmpty()) {
+            selectSQL += " ORDER BY " + order;
+        }
+        try (Connection connection = ds.getConnection();
+        		PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
+        		ResultSet rs = preparedStatement.executeQuery()) {
+            while (rs.next()) {
+                ColoreBean bean = new ColoreBean();
+                bean.setColore(rs.getString("nome"));
+                colori.add(bean);
+            }
+        }
+        return colori;
+    }
 }
