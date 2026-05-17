0da2442fe679e0baaee845d83d907f81c25e9fe4
RaffyMont <raffymont10@gmail.com>
2026-05-17 22:11:33 +0200

    implementazione interfaccia DettagliOrdineDao

---
+++ src/main/java/it/unisa/storage/dao/DettagliOrdineDao.java
@@ -0,0 +1,20 @@
+package it.unisa.storage.dao;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import it.unisa.storage.model.DettagliOrdineBean;
+
+
+public interface DettagliOrdineDao {
+	
+	public void doSave(DettagliOrdineBean dettagli) throws SQLException;
+	
+	public boolean doUpdate(String id_ordine, String id_prodotto) throws SQLException;
+
+    public boolean doDelete(String id_ordine, String id_prodotto) throws SQLException;
+
+    public DettagliOrdineBean doRetrieveByKey(String id_ordine) throws SQLException;
+    
+    public Collection<DettagliOrdineBean> doRetrieveAll(String order) throws SQLException;
+}
