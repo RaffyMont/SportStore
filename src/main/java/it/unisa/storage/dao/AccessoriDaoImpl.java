4331f3d4921142d495c295ff03978c07e04e6aed
RaffyMont <raffymont10@gmail.com>
2026-05-17 16:45:09 +0200

    implementazione metodo doSave nella classe AccessoriDaoImpl

--- src/main/java/it/unisa/storage/dao/AccessoriDaoImpl.java
+++ src/main/java/it/unisa/storage/dao/AccessoriDaoImpl.java
@@ -1,5 +1,7 @@
 package it.unisa.storage.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Collection;
 
@@ -9,14 +11,25 @@ import it.unisa.storage.model.AccessoriBean;
 
 public class AccessoriDaoImpl {
 	
-	private static final String TABLE_NAME = "Scarpe";
+	private static final String TABLE_NAME = "Accessori";
 	private DataSource ds = null;
 
     public AccessoriDaoImpl(DataSource ds) {
         this.ds = ds;
     }
 	
-	public void doSave(AccessoriBean scarpe) throws SQLException;
+	public synchronized void doSave(AccessoriBean accessori) throws SQLException
+	{
+		String insertSQL = "INSERT INTO " + TABLE_NAME
+                + " (id_prodotto, tipo_accessorio, materiale) VALUES (?, ?, ?)";
+        try (Connection connection = ds.getConnection();
+        		PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+            preparedStatement.setString(1, accessori.getId_prodotto());
+            preparedStatement.setString(2, accessori.getTipo_accessori());
+            preparedStatement.setString(3, accessori.getMateriali());
+            preparedStatement.executeUpdate();
+        }
+	}
 
     public boolean doUpdate(AccessoriBean scarpe) throws SQLException;
 
