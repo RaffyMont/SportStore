f2e7bf0ff137f19b54296c356df6d994ec35978b
RaffyMont <raffymont10@gmail.com>
2026-05-17 11:10:35 +0200

    correzione metodo doRetreiveAll e implementazione metodo doRetreiveByCategoria

--- src/main/java/it/unisa/storage/dao/ProdottoDaoImpl.java
+++ src/main/java/it/unisa/storage/dao/ProdottoDaoImpl.java
@@ -123,13 +123,39 @@ public class ProdottoDaoImpl implements ProdottoDao{
                 bean.setCategoria(Categoria.valueOf(rs.getString("categoria").toUpperCase()));
                 bean.setGenere(Genere.valueOf(rs.getString("genere").toUpperCase()));
                 bean.setStock(rs.getInt("stock"));
+                prodotti.add(bean);
+            }
+        }
+        return prodotti;
+	}
+	
+	//Controllare doRetrieveAll, vedere su chat
+	public synchronized List<ProdottoBean> doRetrieveByCategoria(String categoria) throws SQLException
+	{
+		List<ProdottoBean> prodotti = new LinkedList<ProdottoBean>();
+        String selectSQL = "SELECT * FROM " + TABLE_NAME + " WHERE categoria = ?";
+        try (Connection connection = ds.getConnection();
+        		PreparedStatement preparedStatement = connection.prepareStatement(selectSQL)) {
+            preparedStatement.setString(1, categoria);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                while (rs.next()) {
+                	ProdottoBean bean = new ProdottoBean();
+                	bean.setId_prodotto(rs.getString("id_prodotto"));
+                    bean.setModello(rs.getString("modello"));
+                    bean.setDescrizione(rs.getString("descrizione"));
+                    bean.setPrezzo(rs.getDouble("prezzo"));
+                    bean.setAttivo(rs.getBoolean("attivo"));
+                    bean.setMarca(rs.getString("marca"));
+                    bean.setCategoria(Categoria.valueOf(rs.getString("categoria").toUpperCase()));
+                    bean.setGenere(Genere.valueOf(rs.getString("genere").toUpperCase()));
+                    bean.setStock(rs.getInt("stock"));
+                    prodotti.add(bean);
+                }
             }
         }
         return prodotti;
 	}
 	/*
-	public Collection<ProdottoBean> doRetrieveByCategoria(String categoria) throws SQLException;
-	
 	public Collection<ProdottoBean> doRetrieveByGenere(String genere) throws SQLException;
 	
 	public boolean doUpdateStock(String id_utente, int stock) throws SQLException;
