c9452bb04abf14393ea08008b2953bddd30efcab
RaffyMont <raffymont10@gmail.com>
2026-05-17 15:25:25 +0200

    implementazione metodo doUpdate

--- src/main/java/it/unisa/storage/dao/VestitiDaoImpl.java
+++ src/main/java/it/unisa/storage/dao/VestitiDaoImpl.java
@@ -35,7 +35,23 @@ public class VestitiDaoImpl implements VestitiDao{
         }
     }
     
-    public boolean doUpdate(VestitiBean v) throws SQLException;
+    public boolean doUpdate(VestitiBean vestito) throws SQLException
+    {
+    	String sql = "UPDATE " + TABLE_NAME + " SET tipo_vita=?, tessuto=?, stagione=?, categoria=?, tipo_collo=?, manica=?, gamba=? WHERE id_prodotto = ?";
+        try (Connection conn = ds.getConnection();
+        		PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, vestito.getTipovita());
+            ps.setString(2, vestito.getTessuto());
+            ps.setString(3, vestito.getStagione());
+            ps.setString(4, vestito.getCategoria().name().toLowerCase());
+            ps.setString(5, vestito.getTipo_collo());
+            ps.setString(6, vestito.getManica().name().toLowerCase());
+            ps.setString(7, vestito.getGamba().name().toLowerCase());
+            ps.setString(8, vestito.getId_prodotto());
+            int rowsUpdated = ps.executeUpdate();
+            return rowsUpdated != 0;
+        }
+    }
 
     public boolean doDelete(String id_prodotto) throws SQLException;
 
