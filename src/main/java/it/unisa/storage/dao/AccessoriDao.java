21e256a41f2dad4af4db99b098bcf27f28d9f137
RaffyMont <raffymont10@gmail.com>
2026-05-17 16:40:03 +0200

    implementazione interfaccia AccessoriDao

---
+++ src/main/java/it/unisa/storage/dao/AccessoriDao.java
@@ -0,0 +1,18 @@
+package it.unisa.storage.dao;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import it.unisa.storage.model.AccessoriBean;
+
+public interface AccessoriDao {
+	public void doSave(AccessoriBean scarpe) throws SQLException;
+
+    public boolean doUpdate(AccessoriBean scarpe) throws SQLException;
+
+    public boolean doDelete(String id_prodotto) throws SQLException;
+
+    public AccessoriBean doRetrieveByKey(String id_prodotto) throws SQLException;
+    
+    public Collection<AccessoriBean> doRetrieveAll(String order) throws SQLException;
+}
