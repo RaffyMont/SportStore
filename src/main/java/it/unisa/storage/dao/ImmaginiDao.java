3d25ce2b89395550634d150845ad50be805cffdc
RaffyMont <raffymont10@gmail.com>
2026-05-21 23:29:34 +0200

    implementazione interfaccia ImmaginiDao

---
+++ src/main/java/it/unisa/storage/dao/ImmaginiDao.java
@@ -0,0 +1,17 @@
+package it.unisa.storage.dao;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import it.unisa.storage.model.ImmagineBean;
+
+public interface ImmaginiDao {
+	
+	public boolean doSave(ImmagineBean image) throws SQLException;
+	
+	public boolean doUpdate(String pathname) throws SQLException;
+	
+	public boolean doDelete(String pathname) throws SQLException;
+	
+	public Collection<ImmagineBean> doRetrieveAllByIdProdotto(String id_prodotto) throws SQLException;
+}
