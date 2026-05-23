b5d488ac3c5f90d83a41e4edb55b2b6f59141568
RaffyMont <raffymont10@gmail.com>
2026-05-23 18:23:11 +0200

    implementazione metodi doSave e doRetrieveAllByProdotto nella classe SupportoColoreDaoImpl

--- src/main/java/it/unisa/storage/dao/SupportoColoreDaoImpl.java
+++ src/main/java/it/unisa/storage/dao/SupportoColoreDaoImpl.java
@@ -1,11 +1,16 @@
 package it.unisa.storage.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import it.unisa.storage.model.SupportoColoreBean;
+import it.unisa.storage.model.SupportoTagliaBean;
 
 public class SupportoColoreDaoImpl implements SupportoColoreDao{
 	private static final String TABLE_NAME = "SupportoColore";
@@ -17,7 +22,35 @@ public class SupportoColoreDaoImpl implements SupportoColoreDao{
         this.coloreDao = new ColoreDaoImpl(ds);
     }
     
-    public void doSave(SupportoColoreBean supporto) throws SQLException;
+    public synchronized void doSave(SupportoColoreBean supporto) throws SQLException
+    {
+    	String insertSQL = "INSERT INTO " + TABLE_NAME
+                + " (id_prodotto, nome) VALUES (?, ?)";
+        try (Connection connection = ds.getConnection();
+        		PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+            preparedStatement.setString(1, supporto.getId_prodotto().getId_prodotto());
+            preparedStatement.setString(2, supporto.getNome().getColore());
+            preparedStatement.executeUpdate();
+        }
+    }
     
-    public Collection<SupportoColoreBean> doRetrieveAllByProdotto(String id_prodotto) throws SQLException;
+    
+    public synchronized List<SupportoColoreBean> doRetrieveAllByProdotto(String id_prodotto) throws SQLException
+    {
+    	List<SupportoColoreBean> supporti = new LinkedList<SupportoColoreBean>();
+    	String selectSQL = "SELECT c.nome FROM " + TABLE_NAME + " s JOIN Colore c ON c.nome = s.nome WHERE s.id_prodotto = ?";
+    	try (Connection connection = ds.getConnection();
+        		PreparedStatement preparedStatement = connection.prepareStatement(selectSQL)) {
+            preparedStatement.setString(1, id_prodotto);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+	            while (rs.next()) {
+	                SupportoColoreBean bean = new SupportoColoreBean();
+	                bean.setNome(coloreDao.doRetrieveByKey(rs.getString("nome")));
+	                supporti.add(bean);
+	            	}
+                }
+            }
+      
+        return supporti;
+    }
 }
